d02e02eb54afbfb66b5c8aba8ade5ed469299a0a
takumi3110 <57779205+takumi3110@users.noreply.github.com>
2022-04-07 10:36:00 +0900

    state usestateを使用した

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorFullMessage from "./components/ColorFullMessage";
 
 const App = () => {
-  const onClickBUtton = () => alert();
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
 
   return (
     <>
       <h1 style={{ color: "red" }}>hello</h1>
       <ColorFullMessage color="blue">How are you ?</ColorFullMessage>
       <ColorFullMessage color="pink">I'm fine.</ColorFullMessage>
-      <button onClick={onClickBUtton}>button</button>
+      <button onClick={onClickCountUp}>count up</button>
+      <p>{num}</p>
     </>
   );
 };
