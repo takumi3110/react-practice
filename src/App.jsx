ebf15ef1c02b653a5209dc059da6519fe67dd990
takumi3110 <57779205+takumi3110@users.noreply.github.com>
2022-04-07 11:02:19 +0900

    useeffect useEffectを使って、レンダリングの際に特定のものを繰り返さないようにした

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorFullMessage from "./components/ColorFullMessage";
 
 const App = () => {
+  console.log("start");
   const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
@@ -13,7 +30,10 @@ const App = () => {
       <ColorFullMessage color="blue">How are you ?</ColorFullMessage>
       <ColorFullMessage color="pink">I'm fine.</ColorFullMessage>
       <button onClick={onClickCountUp}>count up</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>＼(^o^)／</p>}
     </>
   );
 };
